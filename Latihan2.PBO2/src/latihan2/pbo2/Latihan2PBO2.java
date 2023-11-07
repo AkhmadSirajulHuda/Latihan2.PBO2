887c2f0dd9b58ab385aace14d75c0b271658536d
AkhmadSirajulHuda <@mubarakmhmmd123.com>
2023-11-07 09:35:15 +0800

    first commit

---
+++ Latihan2.PBO2/src/latihan2/pbo2/Latihan2PBO2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan2.pbo2;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Latihan2PBO2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
